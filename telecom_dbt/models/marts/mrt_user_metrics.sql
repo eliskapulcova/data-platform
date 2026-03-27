8e844d5056e623b6b335df706a977b33ec0570e7
pulcovae <extPulcova@dr-max.global>
2026-03-27 12:53:03 +0100

    widen marts table

--- telecom_dbt/models/marts/mrt_user_metrics.sql
+++ telecom_dbt/models/marts/mrt_user_metrics.sql
@@ -4,7 +4,10 @@
 
 select
     user_id,
-    sum(total_duration) as total_duration_all,
-    sum(event_count) as total_events
+    count(*) as total_events,
+    count(distinct event_type) as distinct_event_types,
+    sum(duration) as total_duration,
+    min(event_time) as first_event,
+    max(event_time) as last_event
 from {{ ref('int_user_activity') }}
 group by user_id
