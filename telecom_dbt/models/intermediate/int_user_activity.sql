80e335ad8c55496024a09a8285c89f792b459fd3
pulcovae <extPulcova@dr-max.global>
2026-03-28 20:32:14 +0100

    fixes in dbt

--- telecom_dbt/models/intermediate/int_user_activity.sql
+++ telecom_dbt/models/intermediate/int_user_activity.sql
@@ -1,24 +1,21 @@
 {{ config(
     materialized='incremental',
-    unique_key='user_event_key'
+    unique_key='user_event_id'
 ) }}
 
 with staged as (
-
     select
-    user_id::int,
-    event_type::text,
-    duration::int,
-    event_time::timestamp,
-    -- create composite key for incremental uniqueness
-    user_id || '_' || event_type as user_event_key
-      from {{ ref('stg_user_activity') }}
-
+        user_id::int,
+        event_type::text,
+        duration::int,
+        event_time::timestamp,
+        user_event_id
+    from {{ ref('stg_user_activity') }}
 )
 
 select *
 from staged
-
     {% if is_incremental() %}
-where user_event_key not in (select user_event_key from {{ this }})
+where user_event_id not in (select user_event_id from {{ this }})
     {% endif %}
+order by event_time
--- telecom_dbt/models/staging/stg_user_activity.sql
+++ telecom_dbt/models/staging/stg_user_activity.sql
@@ -1,15 +1,17 @@
 {{ config(
     materialized='incremental',
-    unique_key='user_id'
+    unique_key='user_event_id'
 ) }}
 
 select
     user_id::int,
         event_type::text,
         duration::int,
-        event_time::timestamp
+        event_time::timestamp,
+    -- create a unique key per event for incremental loading
+        user_id || '_' || event_type || '_' || extract(epoch from event_time)::int as user_event_id
 from {{ source('raw', 'raw_user_activity') }}
-
     {% if is_incremental() %}
 where event_time >= (select max(event_time) from {{ this }})
     {% endif %}
+order by event_time
