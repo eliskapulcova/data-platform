785ae9f5dbc5a487980ba9bbfbe5390525679202
pulcovae <extPulcova@dr-max.global>
2026-03-28 18:02:22 +0100

    add kafka producer and consumr - tested

--- ingestion/streaming/consumer.py
+++ ingestion/streaming/consumer.py
@@ -1,5 +1,23 @@
+from kafka import KafkaConsumer
+import json
+
 def main():
     print("Starting Kafka consumer...")
+
+    # Connect to Kafka broker
+    consumer = KafkaConsumer(
+        'user_activity',
+        bootstrap_servers='127.0.0.1:9092',
+        auto_offset_reset='earliest',  # read messages from the beginning
+        group_id='group1',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    )
+
+    # Consume 10 messages as a test
+    for i, message in enumerate(consumer):
+        print(message.value)
+        if i >= 9:  # just read 10 messages
+            break
 
 if __name__ == "__main__":
     main()
--- ingestion/streaming/producer.py
+++ ingestion/streaming/producer.py
@@ -1,5 +1,31 @@
+from kafka import KafkaProducer
+import json
+from datetime import datetime
+import random
+
 def main():
     print("Starting Kafka producer...")
+
+    # Connect to Kafka broker
+    producer = KafkaProducer(
+        bootstrap_servers='127.0.0.1:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    event_types = ["call", "sms", "internet"]
+
+    # Send 10 sample messages
+    for i in range(10):
+        message = {
+            "user_id": random.randint(1, 20),
+            "event_type": random.choice(event_types),
+            "duration": random.randint(1, 300),
+            "event_time": datetime.now().isoformat()
+        }
+        producer.send("user_activity", message)
+
+    producer.flush()
+    print("10 messages sent successfully.")
 
 if __name__ == "__main__":
     main()
